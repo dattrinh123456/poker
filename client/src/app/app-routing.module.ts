13f6bc53c96f5db952d7081a87fc53184b3d6f4b
dattrinh123456 <d.dat29122710@gmail.com>
2023-02-17 19:20:51 +0700

    create homepage and loginpage ui

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { PokerTableComponent } from './poker-table/poker-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: LoginPageComponent,
+    path: 'login'
+  },
+  {
+    component: HomeComponent,
+    path: 'home'
+  },
+  {
+    component: PokerTableComponent,
+    path: 'room/:id'
+  },
+  {
+    component: LoginPageComponent,
+    path: '**'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokerTableComponent } from './poker-table/poker-table.component';
+import { PokerCardComponent } from './poker-card/poker-card.component';
+import { HomeComponent } from './home/home.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokerTableComponent,
+    PokerCardComponent,
+    HomeComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
